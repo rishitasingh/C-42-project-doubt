73dd6923b497a9eac598fb08307fd08b6de8732d
rishitasingh <65798518+rishitasingh@users.noreply.github.com>
2020-09-10 21:23:28 -0700

    Add files via upload

---
+++ C 42 project/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,30 @@
+  hr = hour();
+  mn = minute();
+  sc = second();
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+
+  angleMode(DEGREES);
+
+  
+}
+
+function draw() {
+  background(255,255,255);  
+
+  angleMode(DEGREES);
+  scAngle = map(sc, 0,60,0,360);
+
+  push();
+  rotate(scAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+
+
+
+  drawSprites();
+}
